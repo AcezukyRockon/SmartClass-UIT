cd30cacca31c26cd0dbbf4458d15eeea85efb99e
rock-on <36922874+AcezukyRockon@users.noreply.github.com>
2022-06-17 16:41:28 +0700

    Update index.js

--- webapp/routes/index.js
+++ webapp/routes/index.js
@@ -8,14 +8,25 @@ router.get('/', function(req, res, next) {
   MongoClient.connect(url, function(err, db) {
     if (err) throw err;
     var dbo = db.db("SmartDB");
-    dbo.collection("Trang").find({}).toArray(function(err, result) {
-      if (err) throw err;
-      console.log(result);
-      db.close();
-      res.render('index', { title: 'Express' , data: result});
-    });
-  });
+    var tb = req.query.title;
+    dbo.listCollections().toArray(function(err, collInfos) {  
+    //console.log(collInfos);
+    if (tb == undefined) {
+      //console.log("nothing to retrieve");
+      res.render('index-notable',{title : 'No student chosen', list_tb: collInfos});
+    }
+    else {
+        dbo.collection(tb).find().sort({_id:-1}).limit(10).toArray(function(err, result) {
+          if (err) throw err;
+          //console.log(collInfos);
+          res.render('index', { title: tb , list_tb: collInfos, data: result});
+          //db.close();
+        });
+    }
+    }); 
   
+  });
+
 });
 
 module.exports = router;
