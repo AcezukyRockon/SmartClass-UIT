cbb8d74486d90363727e334a020ecb8c80569b46
rock-on <36922874+AcezukyRockon@users.noreply.github.com>
2022-06-17 17:00:24 +0700

    Update owo.js

--- webapp/owo.js
+++ webapp/owo.js
@@ -4,9 +4,21 @@ var url = "mongodb://localhost:27017/";
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
   var dbo = db.db("SmartDB");
-  dbo.collection("Trang").find({}).toArray(function(err, result) {
+
+
+  if (tb == undefined) {
+    console.log("nothing to retrieve");
+    dbo.listCollections().toArray(function(err, collInfos) {
+    // collInfos is an array of collection info objects that look like:
+    // { name: 'test', options: {} }
+    // console.log(collInfos);
+    res.render('index-notable',{title : 'No table chosen', list_tb: collInfos});
+  });}
+  else { 
+    dbo.collection("Trang").find({}).toArray(function(err, result) {
     if (err) throw err;
     console.log(result);
     db.close();
+  }
   });
 });
